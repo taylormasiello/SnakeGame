62fc607f28e53812f3aed975c10dc7f2429fa438
Taylor Masiello <masiellotay@gmail.com>
2022-04-19 22:38:19 -0700

    Walls, Snake, and Bonuses (at start of level) rendered; 4 bonuses rendered at level start, programmically at random locations and a random bonus image out of 4; snake started with head image and 2 proceeding body images;

--- SnakeGame/Form1.cs
+++ SnakeGame/Form1.cs
@@ -37,7 +37,7 @@ namespace SnakeGame
         GameBoardFields[,] gameBoardField;
         SnakeCoordinates[] snakeXY;
         int snakeLength;
-        Directions directions;
+        Directions direction;
         Graphics g;
 
         public frmSnake()
@@ -50,7 +50,65 @@ namespace SnakeGame
 
         private void frmSnake_Load(object sender, EventArgs e)
         {
+            picGameBoard.Image = new Bitmap(420, 420);
+            g = Graphics.FromImage(picGameBoard.Image);
+            g.Clear(Color.White);
 
+            for (int i = 1; i <= 10; i++)
+            {
+                //top and bottom walls
+                g.DrawImage(imgList.Images[6], i * 35, 0); // i*35 as each iteration will move over 35 pixels, 1 wall block
+                g.DrawImage(imgList.Images[6], i * 35, 385);
+            }
+
+            for (int i = 0; i <= 11; i++)
+            {
+                //left and right walls
+                g.DrawImage(imgList.Images[6], 0, i * 35);
+                g.DrawImage(imgList.Images[6], 385, i * 35);
+            }
+
+            //initial snake body and head
+            snakeXY[0].x = 5; //head
+            snakeXY[0].y = 5;
+            snakeXY[1].x = 5; //first body part
+            snakeXY[1].y = 6;
+            snakeXY[2].x = 5; //second body part
+            snakeXY[2].y = 7;
+
+            //drawing 35 pixels per image
+            g.DrawImage(imgList.Images[5], 5 * 35, 5 * 35); //head
+            g.DrawImage(imgList.Images[4], 5 * 35, 6 * 35); //first body part
+            g.DrawImage(imgList.Images[4], 5 * 35, 7 * 35); //second body part
+
+            gameBoardField[5, 5] = GameBoardFields.Snake;
+            gameBoardField[5, 6] = GameBoardFields.Snake;
+            gameBoardField[5, 7] = GameBoardFields.Snake;
+
+            direction = Directions.Up; //intial direction
+            snakeLength = 3; //intial length
+
+            for (int i = 0; i < 4; i++)
+            {
+                Bonus();
+            }
+        }
+
+        private void Bonus()
+        {
+            int x, y;
+            var imgIndex = rand.Next(0, 4);
+
+            do
+            {
+                //playfield minus walls
+                x = rand.Next(1, 10);
+                y = rand.Next(1, 10);
+            }
+            while (gameBoardField[x, y] != GameBoardFields.Free); //will keep going until finding a gamefield that's free
+
+            gameBoardField[x, y] = GameBoardFields.Bonus; //assigns free gamefield as a bonus gamefield
+            g.DrawImage(imgList.Images[imgIndex], x * 35, y * 35); //draws a random bonus image
         }
     }
 }
